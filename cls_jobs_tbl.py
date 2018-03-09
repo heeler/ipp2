09e8c2a4547ec2202e231dba3b254da8316ed94a
Jamie Sherman <jamies@osxlt7htdf.local>
2018-03-09 14:06:33 -0800

    first commit

---
+++ cls_jobs_tbl.py
@@ -0,0 +1,30 @@
+
+import glob
+import pandas as pd
+
+class JobsTable:
+    """
+    JobsTable collects all the CSV files and creates list of jobs to submit with some filename/path cleanup.
+    """
+
+    def __init__(self, prefs ):
+        """
+        Constructor for JobsTable.
+        :param prefs: the json object containing the contents of the preference file
+        """
+        self.m_prefs = prefs
+        self.m_data = None
+        pass
+
+    def collect_csv_files(self):
+        data_paths = glob.glob(self.m_prefs['data_dir'] + '/*/spreadsheets/*.cleaned.csv')
+
+        self.m_data = list()
+
+        for path in data_paths:
+            data_tmp = pd.read_csv(path)
+            data_tmp['source_data'] = path
+            self.m_data.append(data_tmp)
+
+        self.m_data = pd.concat(self.m_data)
+
